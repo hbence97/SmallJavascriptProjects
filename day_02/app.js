fc60112890c0b3908de40a4e4702c344318f9913
hbence97 <hegyesbence@gmail.com>
2020-03-01 23:22:43 +0100

    Add JavaScript and small CSS corrections

---
+++ day_02/app.js
@@ -0,0 +1,20 @@
+const secondHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".minute-hand")
+const hourHand = document.querySelector(".hour-hand");
+
+function setDate() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const minutes = now.getMinutes();
+  const hours = now.getHours();
+
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  const minuteDegrees = ((minutes / 60) * 360) + 90;
+  const hourDegrees = ((hours / 12) * 360) + 90;
+
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
