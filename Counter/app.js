10d676fecf6da01ae86db8be50544314eb439db9
hbence97 <hegyesbence@gmail.com>
2020-06-14 23:12:36 +0200

    Add counter

---
+++ Counter/app.js
@@ -0,0 +1,6 @@
+const increment = document.querySelector(".increment");
+const counter = document.querySelector(".counter");
+
+increment.addEventListener("click", () => {
+    counter.innerHTML++;
+}) 
