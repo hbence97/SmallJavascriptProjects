b59d2158e6fab8883f15504625803fe3f13fe492
hbence97 <hegyesbence@gmail.com>
2020-08-09 16:10:54 +0200

    Add is it a profitable call

--- IsItAProfitableCall/app.js
+++ IsItAProfitableCall/app.js
@@ -1,10 +1,20 @@
 const button = document.querySelector('.button');
 const outs = document.querySelector('.outs');
-const potOdds = document.querySelector('.pot-odds');
 const checkbox = document.querySelector('.checkbox');
 const reward = document.querySelector('.reward');
 const risk = document.querySelector('.risk');
 const boxChecked = document.querySelector('.checkbox');
+const yourEquity = document.querySelector('.your-equity');
+const yourPotOdds = document.querySelector('.your-pot-odds');
+const equityResult = document.querySelector('.equity-result');
+const potOddsResult = document.querySelector('.pot-odds-result');
+const plusEvCall = document.querySelector('.plus-ev-call');
+const minusEvCall = document.querySelector('.minus-ev-call');
+
+yourEquity.style.display = 'none';
+yourPotOdds.style.display = 'none';
+plusEvCall.style.display = 'none';
+minusEvCall.style.display = 'none';
 
 const notAllInOutsWPercentage = {
     1: 2.13,
@@ -23,7 +33,10 @@ const notAllInOutsWPercentage = {
     14: 29.79,
     15: 31.91,
     16: 34.04,
-    17: 36.17
+    17: 36.17,
+    18: 38.30,
+    19: 40.43,
+    20: 42.55
 }
 
 const AllInOutsWPercentage = {
@@ -43,25 +56,52 @@ const AllInOutsWPercentage = {
     14: 51.16,
     15: 54.12,
     16: 56.98,
-    17: 59.76
+    17: 59.76,
+    18: 62.4,
+    19: 65.0,
+    20: 67.5
 }
 
 button.addEventListener('click', e => {
     e.preventDefault();
 
+    minusEvCall.style.display = 'none';
+    plusEvCall.style.display = 'none';
+    yourEquity.style.display = 'none';
+    yourPotOdds.style.display = 'none';
+
     const outsInput = outs.value;
 
-    let ratioToPercentage = Number(risk.value) / (Number(risk.value) + Number(reward.value));
-    ratioToPercentage *= 100;
-    const roundedRatioToPercentage = Math.round(ratioToPercentage * 10) / 10;
-
-    if (boxChecked.checked) {
-        AllInOutsWPercentage[outsInput] > roundedRatioToPercentage ? console.log('+EV Call') : console.log('-EV Call');
+    if (outsInput === '' || outsInput <= 0 || outsInput > 20 || reward.value === '' || reward.value <= 0 || risk.value === '' || risk.value <= 0) {
+        alert("Please provide valid informations");
     } else {
-        notAllInOutsWPercentage[outsInput] > roundedRatioToPercentage ? console.log('+EV Call') : console.log('-EV Call');
-    }
+        let ratioToPercentage = Number(risk.value) / (Number(risk.value) + Number(reward.value));
+        ratioToPercentage *= 100;
+        const roundedRatioToPercentage = Math.round(ratioToPercentage * 10) / 10;
 
-    outs.value = "";
-    risk.value = "";
-    reward.value = "";
+        yourEquity.style.display = 'block';
+        yourPotOdds.style.display = 'block';
+
+        if (boxChecked.checked) {
+            equityResult.innerHTML = AllInOutsWPercentage[outsInput];
+            if (Number(AllInOutsWPercentage[outsInput]) > roundedRatioToPercentage ) {
+                plusEvCall.style.display = 'block';
+            } else {
+                minusEvCall.style.display = 'block';
+            }
+        } else {
+            equityResult.innerHTML = notAllInOutsWPercentage[outsInput];
+            if (Number(notAllInOutsWPercentage[outsInput]) > roundedRatioToPercentage ) {
+                plusEvCall.style.display = 'block';
+            } else {
+                minusEvCall.style.display = 'block';
+            }
+        }
+
+        potOddsResult.innerHTML = roundedRatioToPercentage;
+
+        outs.value = "";
+        risk.value = "";
+        reward.value = "";
+    }
 });
