d896c6479ac962e3783bd33dd96d5e8119578148
hbence97 <hegyesbence@gmail.com>
2020-10-01 23:00:39 +0200

    Add feat: tetromino movement and rotation with descend

--- Tetris/scripts/app.js
+++ Tetris/scripts/app.js
@@ -1,6 +1,18 @@
 const canvas = document.getElementById("tetris");
 const ctx = canvas.getContext("2d");
 
+document.addEventListener("keydown", e => {
+  if (e.keyCode == 37) {
+    piece.moveLeft();
+  } else if (e.keyCode == 38) {
+    piece.rotate();
+  } else if (e.keyCode == 39) {
+    piece.moveRight();
+  } else if (e.keyCode == 40) {
+    piece.moveDown();
+  }
+});
+
 const _ROW = 20;
 const _COL = 20;
 const square = 20;
@@ -16,15 +28,26 @@ const _PIECES = [
   [J, "#0000f0"],
 ];
 
-/*let board = [];
+let board = [];
 for (let row = 0; row < _ROW; row++) {
   board[row] = [];
   for (let col = 0; col < _COL; col++) {
     board[row][col] = vacant;
   }
-}*/
+}
 
 let piece = new Tetromino(_PIECES[0][0], _PIECES[0][1]);
 
+let dropStart = Date.now();
+function drop() {
+  let now = Date.now();
+  if (now - dropStart > 1000) {
+    piece.moveDown();
+    dropStart = Date.now();
+  }
+  requestAnimationFrame(drop);
+}
+
 drawBoard();
 piece.draw();
+drop();
--- Tetris/scripts/tetromino.js
+++ Tetris/scripts/tetromino.js
@@ -7,6 +7,7 @@ class Tetromino {
     this.x = 0;
     this.y = 0;
   }
+
   draw() {
     for (let row = 0; row < this.activeTetromino.length; row++) {
       for (let col = 0; col < this.activeTetromino.length; col++) {
@@ -15,5 +16,35 @@ class Tetromino {
         }
       }
     }
+  }
+  delete() {
+    for (let row = 0; row < this.activeTetromino.length; row++) {
+      for (let col = 0; col < this.activeTetromino.length; col++) {
+        if (this.activeTetromino[row][col]) {
+          drawSquare(this.x + col, this.y + row, "white");
+        }
+      }
+    }
+  }
+  rotate() {
+    this.delete();
+    this.tetrominoNum = (this.tetrominoNum + 1) % this.tetromino.length;
+    this.activeTetromino = this.tetromino[this.tetrominoNum];
+    this.draw();
+  }
+  moveDown() {
+    this.delete();
+    this.y++;
+    this.draw();
+  }
+  moveRight() {
+    this.delete();
+    this.x++;
+    this.draw();
+  }
+  moveLeft() {
+    this.delete();
+    this.x--;
+    this.draw();
   }
 }
